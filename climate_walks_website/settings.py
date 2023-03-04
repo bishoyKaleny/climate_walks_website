d1c21ce33f88c739c956afdd54b68d53c4d8fe39
bishoyKaleny <118350355+bishoyKaleny@users.noreply.github.com>
2023-03-04 14:03:23 +0100

    Update settings.py

--- climate_walks_website/settings.py
+++ climate_walks_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6*hc7pnz$9%063f_yswk50dy_$e!u-*udn=^_80&0n%y^^+7k!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
